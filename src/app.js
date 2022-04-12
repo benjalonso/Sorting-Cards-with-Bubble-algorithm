8c9d6876918a09835af1c013ca1f2181e8ec3d55
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-12 03:07:21 +0000

    ejercicio casi terminado

--- src/app.js
+++ src/app.js
@@ -3,15 +3,10 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  let button = document.getElementById("button");
-  let pinta1 = document.getElementById("pinta1");
-  let pinta2 = document.getElementById("pinta2");
-  let numeroDeCarta = document.getElementById("numeroDeCarta");
   let draw = document.getElementById("draw");
   let sort = document.getElementById("sort");
-  let numberOfCards = document.getElementById("numberOfCards");
-  let card = document.getElementById("card");
   let contenedorDeCartas = document.getElementById("contenedorDeCartas");
+  let numberOfCards = document.getElementById("numberOfCards");
 
   let pintas = ["♦", "♥", "♠", "♣"];
   let numeros = [
@@ -31,23 +26,98 @@ window.onload = function() {
   ];
 
   draw.addEventListener("click", e => {
-    let cartaAleatoria = e => {
-      let pintaAleatoria = Math.floor(Math.random() * pintas.length);
-      pinta1.innerHTML = pintas[pintaAleatoria];
-      pinta2.innerHTML = pintas[pintaAleatoria];
-      let numeroAleatorio = Math.floor(Math.random() * numeros.length);
-      numeroDeCarta.innerHTML = numeros[numeroAleatorio];
-      if (pintas[pintaAleatoria] == "♠") {
-        pinta1.style.color = "black";
-        pinta2.style.color = "black";
-      } else if (pintas[pintaAleatoria] == "♣") {
-        pinta1.style.color = "black";
-        pinta2.style.color = "black";
-      } else {
-        pinta1.style.color = "red";
-        pinta2.style.color = "red";
+    let newCard = valor => {
+      for (let i = 0; i < valor; i++) {
+        //El codigo de abajo crea la carta
+        let card = document.createElement("div");
+        card.classList.add("card");
+        let pinta1 = document.createElement("span");
+        pinta1.classList.add("pinta1");
+        let pinta2 = document.createElement("span");
+        pinta2.classList.add("pinta2");
+        let cardNumber = document.createElement("p");
+        cardNumber.classList.add("cardNumber");
+        pinta1.innerHTML = "♦";
+        pinta2.innerHTML = "♦";
+        cardNumber.innerHTML = "3";
+        card.appendChild(pinta1);
+        card.appendChild(cardNumber);
+        card.appendChild(pinta2);
+        contenedorDeCartas.appendChild(card);
+        //EL codigo de abajo hace la carta aleatoria
+        let pintaAleatoria = Math.floor(Math.random() * pintas.length);
+        pinta1.innerHTML = pintas[pintaAleatoria];
+        pinta2.innerHTML = pintas[pintaAleatoria];
+        let numeroAleatorio = Math.floor(Math.random() * numeros.length);
+        cardNumber.innerHTML = numeros[numeroAleatorio];
+        if (pintas[pintaAleatoria] == "♠") {
+          pinta1.style.color = "black";
+          pinta2.style.color = "black";
+        } else if (pintas[pintaAleatoria] == "♣") {
+          pinta1.style.color = "black";
+          pinta2.style.color = "black";
+        } else {
+          pinta1.style.color = "red";
+          pinta2.style.color = "red";
+        }
       }
     };
-    console.log(cartaAleatoria());
+
+    newCard(numberOfCards.value);
   });
+  // Evento de creación y ordenamiento de cartas
+  sort.addEventListener("click", e => {
+    // Fuonción para la creación de la carta
+  });
+  const bubbleSort = arr => {
+    for (let wall = arr.length - 1; wall > 0; wall--) {
+      for (let i = 0; i < wall; i++) {
+        if (arr[i] > arr[i + 1]) {
+          let aux = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = aux;
+          let newCard = valor => {
+            for (let i = 0; i < valor; i++) {
+              //El codigo de abajo crea la carta
+              let card = document.createElement("div");
+              card.classList.add("card");
+              let pinta1 = document.createElement("span");
+              pinta1.classList.add("pinta1");
+              let pinta2 = document.createElement("span");
+              pinta2.classList.add("pinta2");
+              let cardNumber = document.createElement("p");
+              cardNumber.classList.add("cardNumber");
+              pinta1.innerHTML = "♦";
+              pinta2.innerHTML = "♦";
+              cardNumber.innerHTML = "3";
+              card.appendChild(pinta1);
+              card.appendChild(cardNumber);
+              card.appendChild(pinta2);
+              contenedorDeCartas.appendChild(card);
+              //EL codigo de abajo hace la carta aleatoria
+              let pintaAleatoria = Math.floor(Math.random() * pintas.length);
+              pinta1.innerHTML = pintas[pintaAleatoria];
+              pinta2.innerHTML = pintas[pintaAleatoria];
+              let numeroAleatorio = Math.floor(Math.random() * numeros.length);
+              cardNumber.innerHTML = numeros[numeroAleatorio];
+              if (pintas[pintaAleatoria] == "♠") {
+                pinta1.style.color = "black";
+                pinta2.style.color = "black";
+              } else if (pintas[pintaAleatoria] == "♣") {
+                pinta1.style.color = "black";
+                pinta2.style.color = "black";
+              } else {
+                pinta1.style.color = "red";
+                pinta2.style.color = "red";
+              }
+            }
+          };
+
+          newCard(numberOfCards.value);
+        }
+      }
+    }
+    return arr;
+  };
+  bubbleSort();
 };
